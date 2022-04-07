de4a94ec5a1af60fccf393f5fad9ffbc6b27cd06
alexander <starchenckoal@yandex.ru>
2022-04-07 20:00:17 +0300

    redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from "react-dom/client"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import {createStore} from "redux"
+import rootReducer from './reducers';
+import { Provider } from 'react-redux';
+const store=createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+createRoot(
   document.getElementById('root')
-);
+).render(<React.StrictMode>
+  <Provider store={store}>
+  <App />
+  </Provider>
+</React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
