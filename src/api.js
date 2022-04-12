03e2e1124219962c3cb0e48ba96a20bcaef42ed4
alexander <starchenckoal@yandex.ru>
2022-04-12 21:26:59 +0300

    card

--- src/api.js
+++ src/api.js
@@ -27,9 +27,11 @@ const currentDay=getCurrentDay()
 const currentDate=`${currentYear}-${currentMonth}-${currentDay}`
 const lastYear=`${currentYear - 1}-${currentMonth}-${currentDay}`
 const nextYear=`${currentYear + 1}-${currentMonth}-${currentDay}`
-const popular_games=`games?key=682f268e9f894d2da318a060bef91231&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`
-const upcoming_games=`games?key=682f268e9f894d2da318a060bef91231&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`
-const newGames=`games?key=682f268e9f894d2da318a060bef91231&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`
+const popular_games=`games?key=1a09bd62c06b4d35b09c6adfcd22a602&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`
+const upcoming_games=`games?key=1a09bd62c06b4d35b09c6adfcd22a602&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`
+const newGames=`games?key=1a09bd62c06b4d35b09c6adfcd22a602&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`
 export const popularGamesURL=()=>`${base_url}${popular_games}`
 export const upcomingGamesURL=()=>`${base_url}${upcoming_games}`
 export const newGamesURL=()=>`${base_url}${newGames}`
+export const gameDetailsURL=(game_id)=>`${base_url}games/${game_id}?key=1a09bd62c06b4d35b09c6adfcd22a602`
+export const gameScreenshotURL=(game_id)=>`${base_url}games/${game_id}/screenshots?key=1a09bd62c06b4d35b09c6adfcd22a602`
